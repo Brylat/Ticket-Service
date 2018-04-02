cd9dea916de955be17a225304dd4dac6033f2a2b
Brylat <brylat8@hotmail.com>
2018-04-02 19:44:06 +0200

    Add JWT configuration

--- src/Ticketomat.Api/Startup.cs
+++ src/Ticketomat.Api/Startup.cs
@@ -1,50 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
 using Ticketomat.Core.Repositories;
 using Ticketomat.Infrastructure.Mappers;
 using Ticketomat.Infrastructure.Repositories.InMemory;
 using Ticketomat.Infrastructure.Services;
 
-namespace Ticketomat.Api
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace Ticketomat.Api {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc()
-                .AddJsonOptions(x => x.SerializerSettings.Formatting = Formatting.Indented);
-            services.AddScoped<IEventRepository,InMemoryEventRepository>();
-            services.AddScoped<IUserRepository,InMemoryUserRepository>();
-            services.AddScoped<IEventService,EventService>();
-            services.AddSingleton(AutoMapperConfig.Initialize());
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddMvc ()
+                .AddJsonOptions (x => x.SerializerSettings.Formatting = Formatting.Indented);
+            services.AddScoped<IEventRepository, InMemoryEventRepository> ();
+            services.AddScoped<IUserRepository, InMemoryUserRepository> ();
+            services.AddScoped<IEventService, EventService> ();
+            services.AddScoped<IUserServices, UserServices> ();
+            services.AddSingleton (AutoMapperConfig.Initialize ());
+
+            services.AddAuthentication (JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer (options => {
+                    options.TokenValidationParameters = new TokenValidationParameters {
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey (Encoding.UTF8.GetBytes (Configuration["Jwt:Key"]))
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
 
+            app.UseAuthentication ();
             app.UseMvc();
         }
     }
